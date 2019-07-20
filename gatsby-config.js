9f59d0bfda1442ceb7f667b985995f374a8eacd0
Oliver Franke <oliver.franke@limebit.de>
2019-07-20 09:36:24 +0200

    update package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,8 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-typescript',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-sass',
       options: {
